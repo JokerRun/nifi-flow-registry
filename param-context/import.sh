1907c414abd9271f9f965e8de42037bffbb5b2f2
rico <565636992@qq.com>
2021-06-28 13:59:24 +0800

    jerry :: update param-context

---
+++ param-context/import.sh
@@ -0,0 +1,19 @@
+./bin/cli.sh nifi import-param-context \
+     -u http://139.224.212.60:8080 \
+     -pcn ddi-transtalent-olap-etl-uat \
+     -i /Users/rico/apps/nifi-registry-0.8.0/flow_storage/param-context/ddi-transtalent-olap-etl/uat/ddi_global_params.json
+
+
+
+./bin/cli.sh nifi import-param-context \
+     -u http://139.224.212.60:8080 \
+     -pcn ddi-transtalent-olap-etl-dev \
+     -i /Users/rico/apps/nifi-registry-0.8.0/flow_storage/param-context/ddi-transtalent-olap-etl/dev/ddi_global_params.json
+
+
+
+./bin/cli.sh nifi import-param-context \
+     -u http://139.224.212.60:8080 \
+     -pcn ddi-transtalent-olap-etl-dev \
+     -i /Users/rico/apps/nifi-registry-0.8.0/flow_storage/param-context/ddi-transtalent-olap-etl/dev/ddi_global_params.json
+
